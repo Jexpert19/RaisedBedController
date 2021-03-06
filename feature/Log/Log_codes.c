66ffa71f2d1cbdebf4b18ee41307011d73c0119b
Jexpert19 <79792854+Jexpert19@users.noreply.github.com>
2021-03-06 17:55:46 +0100

    Delete feature directory

--- feature/Log/Log_codes.c
+++
@@ -1,9 +0,0 @@
-#include "Log_codes.h"
-
-
-// String names of Log_codes
-const char* Log_codeStrings[] = {
-    "system boot",
-    "timetable entry added",
-    "timetable entry deleted"
-};
--- feature/Log/Log_tags.c
+++
@@ -1,9 +0,0 @@
-#include "Log_tags.h"
-
-// String names of Log_tags
-const char* Log_tagStrings[] = {
-    "error  ",
-	"warning",
-	"info   ",
-	"debug  "
-};
