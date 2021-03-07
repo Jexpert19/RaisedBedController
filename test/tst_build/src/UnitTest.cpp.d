871ab16108b66c5400f6f415f73dfc9e6bfabe4f
Jexpert19 <79792854+Jexpert19@users.noreply.github.com>
2021-03-07 14:27:12 +0100

    Delete test/tst_build directory

--- test/tst_build/src/UnitTest.cpp.d
+++
@@ -1,3 +0,0 @@
-build/./src/UnitTest.cpp.o: src/UnitTest.cpp \
- /home/jannis/Dokumente/Datenbank/SoftwareDev/CPP/Catch2-master_Testframework/single_include/catch2/catch.hpp
-/home/jannis/Dokumente/Datenbank/SoftwareDev/CPP/Catch2-master_Testframework/single_include/catch2/catch.hpp:
--- test/tst_build/tst_src/UnitTest.cpp.d
+++
@@ -1,3 +0,0 @@
-tst_build/./tst_src/UnitTest.cpp.o: tst_src/UnitTest.cpp \
- /home/jannis/Dokumente/Datenbank/SoftwareDev/CPP/Catch2-master_Testframework/single_include/catch2/catch.hpp
-/home/jannis/Dokumente/Datenbank/SoftwareDev/CPP/Catch2-master_Testframework/single_include/catch2/catch.hpp:
